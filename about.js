d548aecb38db0514cab81fc5a19c281b5954ad08
Salmiac23232 <122386557+Salmiac23232@users.noreply.github.com>
2024-11-06 22:02:02 +0200

    Refined Text Scroll animation

--- about.js
+++ about.js
@@ -3,12 +3,12 @@ addEventListener("DOMContentLoaded", () =>
     Array.from(document.getElementsByClassName("reveal")).forEach(item =>
         item.innerHTML = `<span>${item.innerText.split(" ").join(" </span><span>")}</span>`),
 ); */
-
+var spans;
 addEventListener("DOMContentLoaded", () => {
     const elements = document.getElementsByClassName("reveal");
     for (const element of elements){
         const words = element.innerText.split(" ");
-        for (j=0; j < words.length; j++){
+        for (let j=0; j < words.length; j++){
             if (j == 0){
                 element.innerHTML = ('<span class="word">' + words[j] + '</span>');
             }
@@ -17,6 +17,24 @@ addEventListener("DOMContentLoaded", () => {
             }
         }
     }
+    revealWords();
     
+  
 });
+
+window.addEventListener("scroll", () => {
+    revealWords();
+    });
+
+function revealWords(){
+    let spans = document.getElementsByClassName("word");
+    for (i = 0; i < spans.length; i++){
+        let {left, top} = spans[i].getBoundingClientRect();
+        top = top - (window.innerHeight * .5);
+        let opacityValue = (top + (left * 0.1)) >  0 ? 0.1 : 1;
+        opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
+        spans[i].style.opacity = opacityValue;
+        
+    }
+}; 
 
